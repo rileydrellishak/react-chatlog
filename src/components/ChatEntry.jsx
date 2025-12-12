0b89ca16df5ab54d16f9ce0c1846472f4604972d
Riley Drellishak <rileydrellishak@gmail.com>
2025-12-12 16:19:49 -0500

    implements toggle between liked and not for heart button!

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -1,15 +1,26 @@
 import './ChatEntry.css';
 import TimeStamp from './TimeStamp';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const ChatEntry = ({ key, sender, body, timeStamp, liked }) => {
+  const [isLiked, setIsLiked] = useState(liked);
+
+  const toggleHeart = () =>{
+    setIsLiked(!isLiked);
+  };
+
+  const displayHeart = () => {
+    return isLiked ? '❤️': '🤍';
+  };
+
   return (
     <article className="chat-entry local" id={key}>
       <h2 className="entry-name">{sender}</h2>
       <section className="entry-bubble">
         <p>{body}</p>
         <p className="entry-time"><TimeStamp time={timeStamp}></TimeStamp></p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={event => toggleHeart()}>{displayHeart()}</button>
       </section>
     </article>
   );
