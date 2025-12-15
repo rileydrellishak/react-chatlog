934edeae0ff0cf78d0f7b0a587ba4c3a74a1739f
Riley Drellishak <rileydrellishak@gmail.com>
2025-12-15 14:51:48 -0500

    initialize colorChoice component

---
+++ src/components/ColorChoice.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ChatLog from './components/ChatLog';
+import data from './data/messages.json';
+import { useState } from 'react';
+
+const ColorChoice = () => {
+  return (
+
+  );
+};
+
+export default ColorChoice;
